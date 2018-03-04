46caf452b56e4ff7638bfcea9cc508845a56c5a9
Stepan Boychenko <bs@appkode.ru>
2018-03-04 12:19:01 +0300

    Init

---
+++ SBExtensions/SBExtensions.h
@@ -0,0 +1,19 @@
+//
+//  SBExtensions.h
+//  SBExtensions
+//
+//  Created by Admin on 04.03.18.
+//  Copyright © 2018 Stepan Boichenko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SBExtensions.
+FOUNDATION_EXPORT double SBExtensionsVersionNumber;
+
+//! Project version string for SBExtensions.
+FOUNDATION_EXPORT const unsigned char SBExtensionsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SBExtensions/PublicHeader.h>
+
+
